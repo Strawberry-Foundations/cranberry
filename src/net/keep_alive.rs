66b722f37abbb9e6428fdefc934373554e5fa524
Juliandev02 <juli.mackenstein@gmail.com>
2024-03-02 19:35:14 +0100

    Fixed keep alive

--- src/net/keep_alive.rs
+++ src/net/keep_alive.rs
@@ -1,15 +1,17 @@
-use std::io::Write;
 use std::net::TcpStream;
 use stblib::colors::{BOLD, C_RESET, RED};
+use stblib::stbm::stbchat::net::OutgoingPacketStream;
+use stblib::stbm::stbchat::packet::ServerPacket;
 
-pub fn keep_alive(mut stream: TcpStream) {
+pub fn keep_alive(mut server: OutgoingPacketStream<TcpStream>) {
     loop {
         stblib::utilities::sleep(30);
-        stream.write_all(b"[#<keepalive.event.sent>]").unwrap_or_else(|_| {
-            eprintln!(
-                "{BOLD}{RED}An error occurred when sending Keep Alive to the server.\n\
+        server.write(ServerPacket::KeepAlive)
+            .unwrap_or_else(|_| {
+                eprintln!(
+                    "{BOLD}{RED}An error occurred when sending Keep Alive to the server.\n\
                 Could it be that the connection to the server has been lost?{C_RESET}"
-            );
-        });
+                );
+            });
     }
 }
